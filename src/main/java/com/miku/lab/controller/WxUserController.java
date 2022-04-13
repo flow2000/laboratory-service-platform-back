b5f8caea36b820b5db0bf7bfb07633aa9bb9e856
梁情晓 <1257322785@qq.com>
2022-04-13 18:53:13 +0800

    添加根据code获得openid和sessionkey

--- src/main/java/com/miku/lab/controller/WxUserController.java
+++ src/main/java/com/miku/lab/controller/WxUserController.java
@@ -4,18 +4,26 @@ package com.miku.lab.controller;/*
  *@version:1.1
  */
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.github.kevinsawicki.http.HttpRequest;
 import com.miku.lab.entity.ArticleSort;
 import com.miku.lab.entity.WxUser;
 import com.miku.lab.entity.vo.ReturnResult;
 import com.miku.lab.service.WxUserService;
 import com.miku.lab.util.AjaxUtil;
 import com.miku.lab.util.Constant;
+import com.miku.lab.util.RedisUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/wx")
@@ -24,6 +32,9 @@ public class WxUserController {
 
     @Autowired
     private WxUserService  wxUserService;
+
+    RedisUtil redisUtil = new RedisUtil();
+
 
     @ApiOperation(value = "获取所有微信用户信息")
     @ApiImplicitParam()
@@ -88,4 +99,27 @@ public class WxUserController {
         }
     }
 
+
+    @ApiOperation(value="获取微信openid")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "js_code",value="登录凭证 js_code",required=true),
+    })
+    @PostMapping("/wxGetUnionId")
+    public ReturnResult getMiniAppOpenId(@RequestParam String js_code) {
+        Map<String, String> data = new HashMap<String, String>();
+        data.put("appid", "wx53295620b3e8adf1");
+        data.put("secret", "f2d8491867cc5926deee9df2dddd7955");
+        data.put("js_code", js_code);
+        data.put("grant_type", "authorization_code");
+        String response = HttpRequest.get("https://api.weixin.qq.com/sns/jscode2session").form(data).body();
+        JSONObject obj= JSON.parseObject(response);//将json字符串转换为json对
+        String openid = obj.getString("openid");
+        String sessionKey = obj.getString("session_key");
+        redisUtil.setString("openid", openid);
+        redisUtil.setString("sessionKey", sessionKey);
+        Map<String, String> result = new HashMap<String, String>();
+        result.put("openid",openid);
+        result.put("sessionKey",sessionKey);
+        return AjaxUtil.success(result, Constant.RESCODE_SUCCESS, 1);
+    }
 }
