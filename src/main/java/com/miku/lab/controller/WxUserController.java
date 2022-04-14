396b44e84921f0af4adada24526b3e1b371607c9
Sort-8 <1982989137@qq.com>
2022-04-14 11:20:09 +0800

    修改appid

--- src/main/java/com/miku/lab/controller/WxUserController.java
+++ src/main/java/com/miku/lab/controller/WxUserController.java
@@ -27,11 +27,11 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/wx")
-@Api(value="WxUserController",tags="微信用户接口")
+@Api(value = "WxUserController", tags = "微信用户接口")
 public class WxUserController {
 
     @Autowired
-    private WxUserService  wxUserService;
+    private WxUserService wxUserService;
 
     RedisUtil redisUtil = new RedisUtil();
 
@@ -39,87 +39,91 @@ public class WxUserController {
     @ApiOperation(value = "获取所有微信用户信息")
     @ApiImplicitParam()
     @GetMapping("/getAllWxUser")
-    public ReturnResult getAllWxUser(){
+    public ReturnResult getAllWxUser() {
         Object map = wxUserService.getAllWxUser();
-        if(map!=null){
-            return AjaxUtil.success(map, Constant.RESCODE_SUCCESS,1);
-        }else{
+        if (map != null) {
+            return AjaxUtil.success(map, Constant.RESCODE_SUCCESS, 1);
+        } else {
             return AjaxUtil.error(Constant.RESCODE_SUCCESS, "获取微信信息失败");
         }
     }
+
     @ApiOperation(value = "更新微信用户个人信息")
     @PostMapping("/updateWxUser")
-    public ReturnResult updateWxUser(@RequestBody WxUser wxUser){
+    public ReturnResult updateWxUser(@RequestBody WxUser wxUser) {
         int updateAffect = wxUserService.updateWxUser(wxUser);
-        if(updateAffect!=0){
-            return AjaxUtil.sucessUpdate(Constant.RESCODE_SUCCESS,"保存成功");
-        }else{
+        if (updateAffect != 0) {
+            return AjaxUtil.sucessUpdate(Constant.RESCODE_SUCCESS, "保存成功");
+        } else {
             return AjaxUtil.error(Constant.RESCODE_MODIFYERROR, "保存失败，请确保你已经登录");
         }
     }
 
     @ApiOperation(value = "更新微信是否推送信息")
     @PostMapping("/updateWxPush")
-    public ReturnResult updateWxPush(@RequestParam String openId,@RequestParam String isReceptMsg,@RequestParam String isReceptPush){
+    public ReturnResult updateWxPush(@RequestParam String openId, @RequestParam String isReceptMsg, @RequestParam String isReceptPush) {
         int updateAffect = wxUserService.updateWxPush(openId, isReceptMsg, isReceptPush);
-        if(updateAffect!=0){
-            return AjaxUtil.sucessUpdate(Constant.RESCODE_SUCCESS,"保存成功");
-        }else{
+        if (updateAffect != 0) {
+            return AjaxUtil.sucessUpdate(Constant.RESCODE_SUCCESS, "保存成功");
+        } else {
             return AjaxUtil.error(Constant.RESCODE_MODIFYERROR, "保存失败，请确保你已经登录");
         }
     }
 
 
-    @ApiOperation(value="添加微信用户接口")
+    @ApiOperation(value = "添加微信用户接口")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "openId",value="微信授权id",required=true),
+            @ApiImplicitParam(name = "openId", value = "微信授权id", required = true),
             @ApiImplicitParam(name = "username", value = "昵称", required = true)
     })
     @PostMapping("/addWxUser")
-    public ReturnResult addWxUser(@RequestParam String openId,@RequestParam String username){
-        int flag = wxUserService.addWxUser(openId,username);
+    public ReturnResult addWxUser(@RequestParam String openId, @RequestParam String username) {
+        int flag = wxUserService.addWxUser(openId, username);
         if (flag == 0) {
-            return AjaxUtil.sucessUpdate(Constant.RESCODE_INSERTERROR,"添加失败，请勿重复添加信息");
-        }else{
-            return AjaxUtil.sucessUpdate(Constant.RESCODE_SUCCESS,"添加成功");
+            return AjaxUtil.sucessUpdate(Constant.RESCODE_INSERTERROR, "添加失败，请勿重复添加信息");
+        } else {
+            return AjaxUtil.sucessUpdate(Constant.RESCODE_SUCCESS, "添加成功");
         }
     }
 
-    @ApiOperation(value="查询单一微信用户接口")
+    @ApiOperation(value = "查询单一微信用户接口")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "openId",value="微信授权id",required=true),
+            @ApiImplicitParam(name = "openId", value = "微信授权id", required = true),
     })
     @PostMapping("/searchWxUser")
-    public ReturnResult searchWxUser(@RequestParam String openId){
+    public ReturnResult searchWxUser(@RequestParam String openId) {
         WxUser wxUser = wxUserService.searchWxUserById(openId);
         if (wxUser != null) {
-            return AjaxUtil.success(wxUser, Constant.RESCODE_SUCCESS,1);
-        }else{
+            return AjaxUtil.success(wxUser, Constant.RESCODE_SUCCESS, 1);
+        } else {
             return AjaxUtil.error(Constant.RESCODE_SUCCESS, "获取微信信息失败");
         }
     }
 
 
-    @ApiOperation(value="获取微信openid")
+    @ApiOperation(value = "获取微信openid")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "js_code",value="登录凭证 js_code",required=true),
+            @ApiImplicitParam(name = "js_code", value = "登录凭证 js_code", required = true),
     })
     @PostMapping("/wxGetUnionId")
     public ReturnResult getMiniAppOpenId(@RequestParam String js_code) {
         Map<String, String> data = new HashMap<String, String>();
-        data.put("appid", "wx53295620b3e8adf1");
-        data.put("secret", "f2d8491867cc5926deee9df2dddd7955");
+        data.put("appid", "wx822356e025c6babf");
+        data.put("secret", "e3a3fab62a3f47042122bc9e8bf421c5");
         data.put("js_code", js_code);
         data.put("grant_type", "authorization_code");
         String response = HttpRequest.get("https://api.weixin.qq.com/sns/jscode2session").form(data).body();
-        JSONObject obj= JSON.parseObject(response);//将json字符串转换为json对
+        JSONObject obj = JSON.parseObject(response);//将json字符串转换为json对
+        if (obj.getString("errcode") != null) {
+            return AjaxUtil.error(Constant.RESCODE_SUCCESS, "获取微信信息失败");
+        }
         String openid = obj.getString("openid");
         String sessionKey = obj.getString("session_key");
         redisUtil.setString("openid", openid);
         redisUtil.setString("sessionKey", sessionKey);
         Map<String, String> result = new HashMap<String, String>();
-        result.put("openid",openid);
-        result.put("sessionKey",sessionKey);
+        result.put("openid", openid);
+        result.put("sessionKey", sessionKey);
         return AjaxUtil.success(result, Constant.RESCODE_SUCCESS, 1);
     }
 }
